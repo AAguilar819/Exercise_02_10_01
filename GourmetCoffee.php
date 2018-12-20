52fe4d4a64279f58b6ed33a6240dbc20402064fa
Unknown <aaguilar819@west-mec.org>
2018-12-19 17:23:02 -0800

    Table Adjustments

--- GourmetCoffee.php
+++ GourmetCoffee.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
-require_once("inc_OnlineStoresDB.php");
+// require_once("inc_OnlineStoresDB.php");
 require_once("class_OnlineStore.php");
-$storeID = array();
+$storeID = "COFFEE";
+$storeInfo = array();
 
 if (class_exists("OnlineStore")) {
     if (isset($_SESSION['currentStore'])) {
@@ -14,9 +15,12 @@ if (class_exists("OnlineStore")) {
     }
     $Store->setStoreID($storeID);
     $storeInfo = $Store->getStoreInformation();
-    echo "<pre>\n";
-    print_r($storeInfo);
-    echo "</pre>\n";
+    if (isset($_GET['ItemToAdd'])) {
+        $Store->addItem();
+    }
+    // echo "<pre>\n";
+    // print_r($storeInfo);
+    // echo "</pre>\n";
 } else {
     $errorMsgs[] = "The <em>OnlineStore</em> class is not available.";
     $Store = NULL;
@@ -43,42 +47,51 @@ GourmetCoffee.php
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0">
     <script src="modernizr.custom.65897.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo $storeInfo['cssFile']; ?>">
 </head>
 
 <body>
-    <h1>Gourmet Coffee</h1>
-    <h2>Description goes here</h2>
+    <h1>
+        <?php echo htmlentities($storeInfo['name']); ?>
+    </h1>
+    <h2>
+        <?php echo htmlentities($storeInfo['description']); ?>
+    </h2>
 
-    <p>Inventory goes here</p>
+    <p>
+        <?php echo htmlentities($storeInfo['welcome']); ?>
+    </p>
     <?php
-    $TableName = "inventory";
-    
-    if (count($errorMsgs) == 0) {
-        $SQLstring = "SELECT * FROM $TableName WHERE storeID='COFFEE'";
-        $QueryResult = $DBConnect->query($SQLstring);
-        if (!$QueryResult) {
-            $errorMsgs[] = "Unable to perform the query.<br> Error code: " . $DBConnect->errno . ": " . $DBConnect->error . ".";
-        } else {
-            echo "<table width='100%'>\n";
-            echo "<tr>";
-            echo "<th>Product</th>\n";
-            echo "<th>Description</th>\n";
-            echo "<th>Price Each</th>\n";
-            echo "</tr>";
-            while (($row = $QueryResult->fetch_assoc()) != NULL) {
-                echo "<tr><td>" . htmlentities($row['name']) . "</td>";
-                echo "<td>" . htmlentities($row['description']) . "</td>";
-                printf("<td>$%.2f</td></tr>\n", $row['price']);
-            }
-            echo "</table>\n";
-            $_SESSION['currentStore'] = serialize($Store);
-        }
-    }
-    if (count($errorMsgs) > 0) {
-        foreach ($errorMsgs as $msg) {
-            echo "<p>$msg</p>\n";
-        }
-    }
+    // $TableName = "inventory";
+    // 
+    // if (count($errorMsgs) == 0) {
+    //     $SQLstring = "SELECT * FROM $TableName WHERE storeID='COFFEE'";
+    //     $QueryResult = $DBConnect->query($SQLstring);
+    //     if (!$QueryResult) {
+    //         $errorMsgs[] = "Unable to perform the query.<br> Error code: " . $DBConnect->errno . ": " . $DBConnect->error . ".";
+    //     } else {
+    //         echo "<table width='100%'>\n";
+    //         echo "<tr>";
+    //         echo "<th>Product</th>\n";
+    //         echo "<th>Description</th>\n";
+    //         echo "<th>Price Each</th>\n";
+    //         echo "</tr>";
+    //         while (($row = $QueryResult->fetch_assoc()) != NULL) {
+    //             echo "<tr><td>" . htmlentities($row['name']) . "</td>";
+    //             echo "<td>" . htmlentities($row['description']) . "</td>";
+    //             printf("<td>$%.2f</td></tr>\n", $row['price']);
+    //         }
+    //         echo "</table>\n";
+    //         $_SESSION['currentStore'] = serialize($Store);
+    //     }
+    // }
+    // if (count($errorMsgs) > 0) {
+    //     foreach ($errorMsgs as $msg) {
+    //         echo "<p>$msg</p>\n";
+    //     }
+    // }
+    $Store->getProductList();
+    $_SESSION['currentStore'] = serialize($Store);
     ?>
 </body>
 
--- class_OnlineStore.php
+++ class_OnlineStore.php
@@ -43,10 +43,10 @@ class OnlineStore {
                     $this->inventory[$row['productID']]['price'] = $row['price'];
                     $this->shoppingCart[$row['productID']] = 0;
                 }
-                echo "<pre>\n";
-                print_r($this->inventory);
-                print_r($this->shoppingCart);
-                echo "</pre>\n";
+                // echo "<pre>\n";
+                // print_r($this->inventory);
+                // print_r($this->shoppingCart);
+                // echo "</pre>\n";
             }
         }
     }
@@ -62,6 +62,47 @@ class OnlineStore {
             }
         }
         return $retval;
+    }
+    
+    public function getProductList() {
+        $retval = false;
+        $subtotal = 0;
+        if (count($this->inventory) > 0) {
+            echo "<table width='100%'>\n";
+            echo "<tr>";
+            echo "<th>Product</th>\n";
+            echo "<th>Description</th>\n";
+            echo "<th>Price Each</th>\n";
+            echo "<th># in Cart</th>\n";
+            echo "<th>Total Price</th>\n";
+            echo "<th>&nbsp;</th>\n";
+            echo "</tr>";
+            foreach ($this->inventory as $ID => $info) {
+                echo "<tr>";
+                echo "<td>" . htmlentities($info['name']) . "</td>\n";
+                echo "<td>" . htmlentities($info['description']) . "</td>\n";
+                printf("<td calss='currency'>$%.2f</td>\n", $info['price']);
+                echo "<td class='currency'>" . $this->shoppingCart[$ID] . "</td>\n";
+                printf("<td class='currency'>$%.2f</td>\n", $info['price'] * $this->shoppingCart[$ID]);
+                echo "<td><a href='" . $_SERVER['SCRIPT_NAME'] . "?PHPSESSID=" . session_id() . "&ItemToAdd=$ID'>Add Item</a></td>";
+                $subtotal += ($info['price'] * $this->shoppingCart[$ID]);
+                echo "</tr>\n";
+            }
+            echo "<tr>";
+            echo "<td colspan='4'>Subtotal</td>";
+            //printf("<td calss='currency'>$%.2f</td>\n", $subtotal);
+            echo "</tr>\n";
+            echo "</table>\n";
+            $retval = true;
+        }
+        return($retval);
+    }
+    
+    public function addItem() {
+        $prodID = $_GET['ItemToAdd'];
+        if (array_key_exists($prodID, $this->shoppingCart)) {
+            $this->shoppingCart[$prodID] += 1;
+        }
     }
 }
 
